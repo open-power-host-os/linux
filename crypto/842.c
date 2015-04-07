4a0f7a9f06c860f37c67b9691dc166ed9db21766
Dan Streetman <ddstreet@us.ibm.com>
2015-04-07 14:55:49 -0400

    drivers/crypto/nx: add NX-842 platform frontend driver
    Add NX-842 frontend that allows using either the pSeries platform
    or PowerNV platform driver for the NX-842 hardware.  Update the
    MAINTAINERS file to include the new filenames.
    
    Signed-off-by: Dan Streetman <ddstreet@ieee.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- crypto/842.c
+++ crypto/842.c
@@ -52,7 +52,7 @@ static int nx842_init(struct crypto_tfm *tfm)
 	struct nx842_ctx *ctx = crypto_tfm_ctx(tfm);
 	int wmemsize;
 
-	wmemsize = max_t(int, nx842_get_workmem_size(), LZO1X_MEM_COMPRESS);
+	wmemsize = max_t(int, NX842_MEM_COMPRESS, LZO1X_MEM_COMPRESS);
 	ctx->nx842_wmem = kmalloc(wmemsize, GFP_NOFS);
 	if (!ctx->nx842_wmem)
 		return -ENOMEM;
