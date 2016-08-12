b67457e638c128bda3df9308a7f7e79b50754aae
Paul Mackerras <paulus@ozlabs.org>
2016-08-12 14:30:53 +1000

    tipc: fix an infoleak in tipc_nl_compat_link_dump
    This fixes the information leak that is fixed upstream by commits
    5d2be1422e02 ("tipc: fix an infoleak in tipc_nl_compat_link_dump",
    2016-06-02) and 55e77a3e8297 ("tipc: fix nl compat regression for link
    statistics", 2016-07-01).  The code has changed very substantially
    upstream, so this just memsets the destination array to 0 before
    copying the string in with strcpy, as the simplest way to prevent
    the information leak.
    
    Fix for CVE-2016-5243.
    
    Signed-off-by: Paul Mackerras <paulus@ozlabs.org>

--- net/tipc/node.c
+++ net/tipc/node.c
@@ -429,6 +429,7 @@ struct sk_buff *tipc_node_get_links(const void *req_tlv_area, int req_tlv_space)
 				continue;
 			link_info.dest = htonl(n_ptr->addr);
 			link_info.up = htonl(tipc_link_is_up(n_ptr->links[i]));
+			memset(link_info.str, 0, TIPC_MAX_LINK_NAME);
 			strcpy(link_info.str, n_ptr->links[i]->name);
 			tipc_cfg_append_tlv(buf, TIPC_TLV_LINK_INFO,
 					    &link_info, sizeof(link_info));
